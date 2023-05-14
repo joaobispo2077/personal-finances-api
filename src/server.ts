3ce51cdc07571a20618d53c61e34ac8630d47051
João Bispo <joaobispo2077@gmail.com>
2023-05-14 15:41:32 -0300

    feat: write cookie as session_id

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,13 @@ import 'dotenv/config'
 import fastify from 'fastify'
 import { configs } from './configs'
 import { makeTransactionsRoutes } from './routes/transactions'
+import cookie from '@fastify/cookie'
 
 const { PORT } = configs
 
 const app = fastify()
+
+app.register(cookie)
 
 app.register(makeTransactionsRoutes, {
   prefix: 'transactions',
