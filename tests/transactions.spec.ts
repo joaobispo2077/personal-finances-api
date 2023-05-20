1575627695a61b2531049f1fb7af1444cf520bb5
João Bispo <joaobispo2077@gmail.com>
2023-05-20 15:54:05 -0300

    test: should be able to list all transactions

--- tests/transactions.spec.ts
+++ tests/transactions.spec.ts
@@ -20,4 +20,35 @@ describe('Transactions routes', () => {
 
     expect(response.statusCode).toBe(201)
   })
+
+  it('should be able to list all transactions', async () => {
+    const responseCreated = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'Gym membership',
+        amount: 100,
+        type: 'debit',
+      })
+
+    const cookies = responseCreated.get('Set-Cookie')
+
+    const response = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+
+    const { transactions } = response.body
+
+    expect(response.statusCode).toBe(200)
+    expect(transactions).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          id: expect.any(String),
+          created_at: expect.any(String),
+          session_id: expect.any(String),
+          title: 'Gym membership',
+          amount: -100,
+        }),
+      ]),
+    )
+  })
 })
