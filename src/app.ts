d0bcf10041ed20dd5a31810f595b26fe14ff48c0
João Bispo <joaobispo2077@gmail.com>
2023-07-27 15:30:29 -0300

    feat: setup swagger docs

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,34 @@
 import fastify from 'fastify'
 import { makeTransactionsRoutes } from './routes/transactions'
 import cookie from '@fastify/cookie'
+import fastifySwagger, { SwaggerOptions } from '@fastify/swagger'
+import fastifySwaggerUi, { FastifySwaggerUiOptions } from '@fastify/swagger-ui'
+import pkg from '../package.json'
+import { configs } from './configs'
 
 const app = fastify()
+
+const swaggerOptions: SwaggerOptions = {
+  swagger: {
+    info: {
+      title: pkg.name,
+      description: pkg.description,
+      version: pkg.version,
+    },
+    host: configs.HOST,
+    schemes: ['http', 'https'],
+    consumes: ['application/json'],
+    produces: ['application/json'],
+    tags: [{ name: 'Default', description: 'Default' }],
+  },
+}
+
+const swaggerUiOptions: FastifySwaggerUiOptions = {
+  routePrefix: '/docs',
+}
+
+app.register(fastifySwagger, swaggerOptions)
+app.register(fastifySwaggerUi, swaggerUiOptions)
 
 app.register(cookie)
 
